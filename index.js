511fe3e7a4f18a9f03e092313b080d9000da9fb2
jimhome_mac <jsr1611@outlook.com>
2023-06-14 19:05:46 +0900

    project exercisetracker done

--- index.js
+++ index.js
@@ -60,58 +60,79 @@ app.route("/api/users")
         }
     });
 
+
     app.route("/api/users/:_id/exercises").post(async (req, res) => {
         let id = req.params._id;
         let desc = req.body.description;
         let period = req.body.duration;
         let dateStr =
-            (req.body.date == null || req.body.date == "undefined" || req.body.date == "")
-                ? new Date().toISOString().substring(0,10)
-                : req.body.date;
-      console.log(req.method + ' exercises , date: ' + dateStr + '(' + req.body.date + ')');
-      console.log(id+', '+desc+', '+period+', '+dateStr);
+          (req.body.date == null || req.body.date == "undefined" || req.body.date == "")
+            ? new Date()
+            : new Date(req.body.date);
+        console.log(req.method + ' exercises , date: ' + dateStr + '(' + req.body.date + ')');
+        console.log(id + ', ' + desc + ', ' + period + ', ' + dateStr);
         try {
-            let user = await User.findById({ _id: id });
-            let exerCise = new Exercise({
-                username: user.username,
-                description: desc,
-                duration: period,
-                date: dateStr,
-            });
-            let data = await exerCise.save();
-            res.json({
-                username: user.username,
-                description: desc,
-              duration: Number(period),
-              date: new Date(dateStr).toDateString(),
-              _id: id
-            });
+          let user = await User.findById({ _id: id });
+          let exerCise = new Exercise({
+            username: user.username,
+            description: desc,
+            duration: period,
+            date: dateStr,
+          });
+          let data = await exerCise.save();
+          res.json({
+            username: user.username,
+            description: desc,
+            duration: Number(period),
+            date: dateStr.toDateString(),
+            _id: id
+          });
         } catch (err) {
-            console.log(err);
-            res.send(err);
+          console.log(err);
+          res.send(err);
         }
-    });
-    
-    app.get("/api/users/:_id/logs", async (req, res) => {
-      console.log(req.method + ' logs ');
+      });
+      
+      app.get("/api/users/:_id/logs", async (req, res) => {
+        console.log(req.method + ' logs ');
+        let lim = req.query.limit;
+        let from = req.query.from;
+        let to = req.query.to;
+        let queryStr = '{';
+        if(from != null && from != ''){
+          queryStr = `$gt: new Date('${from}')`;
+        }
+        if(to != null && to != ''){
+          queryStr += `, $lt: new Date('${to}')`;
+        }
+        queryStr += '}';
+        
+        
+        
         try {
-            let user = await User.findById(req.params._id);
-            let logs = await Exercise.find({ username: user.username }, {username: 0, _id: 0});
-          logs.forEach(x => {
-              x.date = new Date(x.date).toDateString();
-            });
+          let user = await User.findById(req.params._id);
+          let logs = await Exercise.find({ username: user.username}, { username: 0, _id: 0 }).limit(lim);
+          let mylogs = [];
+            logs.forEach((x, index) => {
+            let obj = {
+              description: x.description,
+              duration: x.duration,
+              date: x.date.toDateString()
+            };
+              mylogs.push(obj);
+          });
           console.log(logs);
-            res.json({
-              _id: user._id,  
-              username: user.username,
-                count: logs.length,
-                log: logs
-            });
+          res.json({
+            _id: user._id,
+            username: user.username,
+            count: logs.length,
+            log: mylogs
+          });
         } catch (err) {
-            console.log(err);
-            res.send(err);
+          console.log(err);
+          res.send(err);
         }
-    });
+      });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log("Your app is listening on port " + listener.address().port);
